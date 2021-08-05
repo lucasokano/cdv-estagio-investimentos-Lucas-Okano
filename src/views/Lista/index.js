08fce6a6678b8a661a71e0957b8b7deda31f73df
lucasokano <luc4sko@hotmail.com>
2021-08-04 21:26:11 -0300

    lista conectada com a api

--- src/views/Lista/index.js
+++ src/views/Lista/index.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import { assertTSBigIntKeyword } from '@babel/types'
+import { ClientSession } from 'mongodb'
+import React, { useState } from 'react'
+import api from '../../api'
 
 const Lista = () => {
+
+    const[cliente, setCliente] = useState(0);
+
+    async componentDidMount() {
+        const response = await api.get('cliente');
+        
+        this.setCliente(response.data); 
+    }
+
     return(
         <div>
             <h1>Lista de Clientes</h1>
-            <p>aqui está a lista de clientes</p>
-        </div>
+            <ul>
+                {cliente.map(cliente =>{
+                    <li key={cliente._key}>
+                    <h2>Nome:{cliente.nome}</h2>
+                    <h2>Nome:{cliente.cpf}</h2>
+                    <h2>Nome:{cliente.etapa}</h2>
+                    </li>
+                })}
+            </ul>
+       </div>
     )
 }
 
